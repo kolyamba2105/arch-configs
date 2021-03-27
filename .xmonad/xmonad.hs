5bfbb0be275ba891902cee8a6c09d117f1bf8b91
kolyamba2105 <haravetskyy@gmail.com>
2021-03-27 10:43:23 +0200

    Add TreeSelect to XMonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,12 @@
 import Data.Map (fromList)
 import Data.Maybe (fromJust)
 import Data.Monoid
+import Data.Tree
 import Graphics.X11.ExtraTypes.XF86
 import System.Exit
 import System.IO
 import XMonad
+import XMonad.Actions.TreeSelect
 import XMonad.Hooks.DynamicLog (PP(..), dynamicLogWithPP, shorten, wrap, xmobarColor, xmobarPP)
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Grid
@@ -19,8 +21,6 @@ import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
 
 myTerminal = "alacritty"
-
-myBrowser = "firefox"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
@@ -42,31 +42,32 @@ myNormalBorderColor = "#424242"
 
 myFocusedBorderColor = "#18ffff"
 
+myFont = "xft:JetBrainsMono Nerd Font:pixelsize=14:antialias=true:hinting=true"
+
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
 myKeys =
   [ ("M-S-<Return>", spawn myTerminal)
+    -- Tree select actions
+  , ("M-a", actions treeSelectConfig)
     -- Launch rofi
   , ("M-p", spawn "rofi -show run")
-    -- Launch default browser
-  , ("M-f", spawn myBrowser)
-    -- Launch default browser (firefox in this case) in private mode
-  , ("M-S-f", spawn (myBrowser ++ " --private-window"))
+    -- Launch firefox
+  , ("M-f", spawn "firefox")
+    -- Launch firefox in private mode
+  , ("M-S-f", spawn "firefox --private-window")
+    -- Launch chromium
+  , ("M-g", spawn "chromium")
     -- Launch chromium in incognito mode
-  , ("M-g", spawn "chromium --incognito")
+  , ("M-S-g", spawn "chromium --incognito")
     -- Launch PulseMixer
   , ("M-s", spawn (myTerminal ++ " -e pulsemixer"))
-    -- Increase brightness by 10%
-  , ("M-<F6>", spawn "xbacklight -inc 10")
-    -- Decrease brightness by 10%
-  , ("M-<F5>", spawn "xbacklight -dec 10")
-  , ("M-<F10>", spawn "toggle-bluetooth")
-   --Take a screenshot of entire display
+    -- Take a screenshot of entire display
   , ("M-<Print>", spawn "scrot ~/Pictures/Screenshots/screen-%Y-%m-%d-%H-%M-%S.png -d 1")
-   --Take a screenshot of focused window
+    -- Take a screenshot of focused window
   , ("M-C-<Print>", spawn "scrot ~/Pictures/Screenshots/window-%Y-%m-%d-%H-%M-%S.png -d 1-u")
-   --Take a screenshot of chosen area
+    -- Take a screenshot of chosen area
   , ("M-S-<Print>", spawn "scrot ~/Pictures/Screenshots/area-%Y-%m-%d-%H-%M-%S.png -s 1-u")
     -- Close focused window
   , ("M-S-c", kill)
@@ -100,16 +101,10 @@ myKeys =
   , ("M-.", sendMessage (IncMasterN (-1)))
     -- Toggle the status bar gap
   , ("M-b", sendMessage ToggleStruts)
-    -- Lock screen
-  , ("M-S-l", spawn "slock")
-    -- Suspend system
-  , ("M-S-s", spawn "systemctl suspend")
     -- Quit xmonad (don't want to accidentally close session)
   , ("M-S-q", return ())
     -- Restart xmonad
   , ("M-q", spawn "xmonad --recompile; xmonad --restart")
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
-  , ("M-S-/", spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
   ]
 
 ------------------------------------------------------------------------
@@ -152,7 +147,7 @@ tabbedLayout = renamed [Replace "Tabbed"] (noBorders (tabbedBottomAlways shrinkT
 
 myTabbedTheme =
   def
-    { fontName = "xft:JetBrainsMono Nerd Font:pixelsize=14:antialias=true:hinting=true"
+    { fontName = myFont
     , activeColor = "#b2ff59"
     , inactiveColor = "#424242"
     , activeBorderColor = "#b2ff59"
@@ -168,6 +163,53 @@ mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
 defaultSpacing :: l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 defaultSpacing = mySpacing 4
+
+-- Tree select
+treeSelectConfig =
+  TSConfig
+    { ts_hidechildren = True
+    , ts_background = 0xea333333
+    , ts_font = myFont
+    , ts_node = (0xff000000, 0xff00bfa5)
+    , ts_nodealt = (0xff000000, 0xff1de9b6)
+    , ts_highlight = (0xffffffff, 0xff00695c)
+    , ts_extra = 0xff000000
+    , ts_node_width = 200
+    , ts_node_height = 30
+    , ts_originX = 0
+    , ts_originY = 0
+    , ts_indent = 80
+    , ts_navigate = defaultNavigation
+    }
+
+empty = ""
+
+parentAction name children = Node (TSNode name empty (return ())) children
+
+childAction name action = Node (TSNode name empty action) []
+
+actions config =
+  treeselectAction
+    config
+    [ parentAction
+        "System"
+        [ childAction "Lock" (spawn "slock")
+        , childAction "Suspend" (spawn "systemctl suspend")
+        , childAction "Kill session" (io (exitWith ExitSuccess))
+        , childAction "Shutdown" (spawn "shutdown now")
+        , childAction "Reboot" (spawn "reboot")
+        ]
+    , parentAction
+        "Brightness"
+        [ childAction "100%" (spawn "xbacklight -set 100")
+        , childAction "80%" (spawn "xbacklight -set 80")
+        , childAction "60%" (spawn "xbacklight -set 60")
+        , childAction "40%" (spawn "xbacklight -set 40")
+        , childAction "20%" (spawn "xbacklight -set 20")
+        , childAction "0%" (spawn "xbacklight -set 0")
+        ]
+    , childAction "Bluetooth" (spawn "toggle-bluetooth")
+    ]
 
 ------------------------------------------------------------------------
 -- Window rules
@@ -259,57 +301,3 @@ defaultSettings xMobar =
     , logHook = myLogHook <+> (xmobarPrettyPrinting xMobar)
     , startupHook = myStartupHook
     }
-
--- | Finally, a copy of the default bindings in simple textual tabular format.
-help :: String
-help =
-  unlines
-    [ "The default modifier key is 'alt'. Default keybindings:"
-    , ""
-    , "-- launching and killing programs"
-    , "mod-Shift-Enter  Launch xterminal"
-    , "mod-p            Launch dmenu"
-    , "mod-Shift-p      Launch gmrun"
-    , "mod-Shift-c      Close/kill the focused window"
-    , "mod-Space        Rotate through the available layout algorithms"
-    , "mod-Shift-Space  Reset the layouts on the current workSpace to default"
-    , "mod-n            Resize/refresh viewed windows to the correct size"
-    , ""
-    , "-- move focus up or down the window stack"
-    , "mod-Tab        Move focus to the next window"
-    , "mod-Shift-Tab  Move focus to the previous window"
-    , "mod-j          Move focus to the next window"
-    , "mod-k          Move focus to the previous window"
-    , "mod-m          Move focus to the master window"
-    , ""
-    , "-- modifying the window order"
-    , "mod-Return   Swap the focused window and the master window"
-    , "mod-Shift-j  Swap the focused window with the next window"
-    , "mod-Shift-k  Swap the focused window with the previous window"
-    , ""
-    , "-- resizing the master/slave ratio"
-    , "mod-h  Shrink the master area"
-    , "mod-l  Expand the master area"
-    , ""
-    , "-- floating layer support"
-    , "mod-t  Push window back into tiling; unfloat and re-tile it"
-    , ""
-    , "-- increase or decrease number of windows in the master area"
-    , "mod-comma  (mod-,)   Increment the number of windows in the master area"
-    , "mod-period (mod-.)   Deincrement the number of windows in the master area"
-    , ""
-    , "-- quit, or restart"
-    , "mod-Shift-q  Quit xmonad"
-    , "mod-q        Restart xmonad"
-    , "mod-[1..9]   Switch to workSpace N"
-    , ""
-    , "-- Workspaces & screens"
-    , "mod-Shift-[1..9]   Move client to workspace N"
-    , "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3"
-    , "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3"
-    , ""
-    , "-- Mouse bindings: default actions bound to mouse events"
-    , "mod-button1  Set the window to floating mode and move by dragging"
-    , "mod-button2  Raise the window to the top of the stack"
-    , "mod-button3  Set the window to floating mode and resize by dragging"
-    ]
