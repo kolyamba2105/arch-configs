392a76e2990c8e5ac69898f9c16fcd0822f96f2c
kolyamba2105 <haravetskyy@gmail.com>
2021-03-27 11:34:22 +0200

    Remove redundant comments and variables in XMonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -22,30 +22,9 @@ import XMonad.Util.SpawnOnce
 
 myTerminal = "alacritty"
 
--- Whether focus follows the mouse pointer.
-myFocusFollowsMouse :: Bool
-myFocusFollowsMouse = True
-
--- Whether clicking on a window to focus also passes the click to the window
-myClickJustFocuses :: Bool
-myClickJustFocuses = False
-
--- Width of the window border in pixels.
-myBorderWidth = 2
-
-myModMask = mod4Mask
-
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
--- Border colors for unfocused and focused windows, respectively.
-myNormalBorderColor = "#424242"
-
-myFocusedBorderColor = "#18ffff"
-
 myFont = "xft:JetBrainsMono Nerd Font:pixelsize=14:antialias=true:hinting=true"
 
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
+-- Key bindings
 --
 myKeys =
   [ ("M-S-<Return>", spawn myTerminal)
@@ -101,14 +80,13 @@ myKeys =
   , ("M-.", sendMessage (IncMasterN (-1)))
     -- Toggle the status bar gap
   , ("M-b", sendMessage ToggleStruts)
-    -- Quit xmonad (don't want to accidentally close session)
+    -- Quit xmonad (don't want to accidentally kill session)
   , ("M-S-q", return ())
     -- Restart xmonad
   , ("M-q", spawn "xmonad --recompile; xmonad --restart")
   ]
 
-------------------------------------------------------------------------
--- Mouse bindings: default actions bound to mouse events
+-- Mouse bindings
 --
 myMouseBindings (XConfig {XMonad.modMask = modm}) =
   fromList $
@@ -121,15 +99,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) =
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
   ]
 
-------------------------------------------------------------------------
--- Layouts:
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
+-- Layouts
 --
 defaultTallLayout = Tall nMaster delta ratio
   where
@@ -165,6 +135,7 @@ defaultSpacing :: l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 defaultSpacing = mySpacing 4
 
 -- Tree select
+--
 treeSelectConfig =
   TSConfig
     { ts_hidechildren = True
@@ -211,41 +182,20 @@ actions config =
     , childAction "Bluetooth" (spawn "toggle-bluetooth")
     ]
 
-------------------------------------------------------------------------
 -- Window rules
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
 --
 myManageHook = composeAll []
 
-------------------------------------------------------------------------
 -- Event handling
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
 --
 myEventHook = mempty
 
-------------------------------------------------------------------------
 -- Status bars and logging
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.DynamicLog' extension for examples.
 --
 myLogHook = return ()
 
-------------------------------------------------------------------------
 -- Startup hook
+--
 screenLayout = "set-screenlayout"
 
 keyboardLayout = "setxkbmap -layout us,pl,ru,ua -option grp:alt_shift_toggle"
@@ -269,48 +219,46 @@ myStartupHook = do
   spawn wallpapers
   spawn compositor
 
-------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
--- Run xmonad with the settings you specify. No need to modify this.
+-- Main
 --
 main = do
   xMobar <- spawnPipe "xmobar -x 0"
   xmonad $ docks ((defaultSettings xMobar) `additionalKeysP` myKeys)
 
--- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will
--- use the defaults defined in xmonad/XMonad/Config.hs
---
--- No need to modify this.
 xmobarPrettyPrinting xMobar =
   dynamicLogWithPP
     xmobarPP
       { ppOutput = hPutStrLn xMobar
-      , ppCurrent = xmobarColor "#b2ff59" "" . wrap "[" "]" -- Current workspace in xmobar
-      , ppVisible = xmobarColor "#18ffff" "" -- Visible but not current workspace
-      , ppHidden = xmobarColor "#40c4ff" "" . wrap "*" "" -- Hidden workspaces in xmobar
-      , ppHiddenNoWindows = xmobarColor "#ff4081" "" -- Hidden workspaces (no windows)
-      , ppTitle = xmobarColor "#64ffda" "" . shorten 50 -- Title of active window in xmobar
+        -- Current workspace in xmobar
+      , ppCurrent = xmobarColor "#b2ff59" "" . wrap "[" "]"
+        -- Visible but not current workspace
+      , ppVisible = xmobarColor "#18ffff" ""
+        -- Hidden workspaces in xmobar
+      , ppHidden = xmobarColor "#40c4ff" "" . wrap "*" ""
+        -- Hidden workspaces (no windows)
+      , ppHiddenNoWindows = xmobarColor "#ff4081" ""
+        -- Title of active window in xmobar
+      , ppTitle = xmobarColor "#64ffda" "" . shorten 50
       , ppLayout = xmobarColor "#eeff41" ""
-      , ppSep = "<fc=#eeeeee> | </fc>" -- Separators in xmobar
-      , ppUrgent = xmobarColor "#ff5252" "" . wrap "!" "!" -- Urgent workspace
-      , ppExtras = [] -- # of windows current workspace
+        -- Separators in xmobar
+      , ppSep = "<fc=#eeeeee> | </fc>"
+        -- Urgent workspace
+      , ppUrgent = xmobarColor "#ff5252" "" . wrap "!" "!"
+        -- # of windows current workspace
+      , ppExtras = []
       }
 
 defaultSettings xMobar =
   def
-      -- simple stuff
     { terminal = myTerminal
-    , focusFollowsMouse = myFocusFollowsMouse
-    , clickJustFocuses = myClickJustFocuses
-    , borderWidth = myBorderWidth
-    , modMask = myModMask
-    , workspaces = myWorkspaces
-    , normalBorderColor = myNormalBorderColor
-    , focusedBorderColor = myFocusedBorderColor
-      -- key bindings
+    , focusFollowsMouse = True
+    , clickJustFocuses = False
+    , borderWidth = 2
+    , modMask = mod4Mask
+    , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    , normalBorderColor = "#424242"
+    , focusedBorderColor = "#18ffff"
     , mouseBindings = myMouseBindings
-      -- hooks, layouts
     , layoutHook = myLayout
     , manageHook = manageDocks <+> myManageHook
     , handleEventHook = myEventHook
