43a257d23fba495a58ec249cf0b1b7c7add5ffcc
kolyamba2105 <haravetskyy@gmail.com>
2021-05-07 18:31:19 +0300

    Turn conky off

--- .screenlayout/dual-monitor.sh
+++ .screenlayout/dual-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --mode 1920x1080 --pos 2560x360 --rotate normal --output HDMI1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
