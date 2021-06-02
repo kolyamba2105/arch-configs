d040e0ae1059bced9f7a27ac7612cb197a68fc8f
kolyamba2105 <haravetskyy@gmail.com>
2021-06-02 13:48:06 +0300

    Add telegram ScratchPad and organize XMonad keybindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -37,106 +37,62 @@ myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 ignoredWorkspaces = ["NSP"]
 
 -- Key bindings
-myKeys =
-  [ ("M-S-<Return>", spawn myTerminal),
-    -- Launch terminal with transparent background
-    ("M-C-<Return>", spawn myTransparentTerminal),
-    -- Launch shell prompt
-    ("M-p", shellPrompt myPromptConfig),
-    -- Launch firefox
-    ("M-f", spawn "firefox"),
-    -- Launch firefox in private mode
-    ("M-S-f", spawn "firefox --private-window"),
-    -- Launch brave
-    ("M-g", spawn "brave"),
-    -- Launch brave in incognito mode
-    ("M-S-g", spawn "brave --incognito"),
-    -- Open terminal ScratchPad
-    ("M-M1-<Return>", openScratchPad "terminal"),
-    -- Open HTOP ScratchPad
-    ("M-S-t", openScratchPad "htop"),
-    -- Open ranger ScratchPad
-    ("M-d", openScratchPad "ranger"),
-    -- Open mixer ScratchPad
-    ("M-s", openScratchPad "mixer"),
-    -- Open slack ScratchPad
-    ("M-S-s", openScratchPad "slack"),
-    -- Take a screenshot of entire display
-    ("M-<Print>", spawn "scrot -q 100 ~/Pictures/Screenshots/screen-%Y-%m-%d-%H-%M-%S.png"),
-    -- Take a screenshot of focused window
-    ("M-C-<Print>", spawn "scrot -u -q 100 ~/Pictures/Screenshots/window-%Y-%m-%d-%H-%M-%S.png"),
-    -- Take a screenshot of chosen area
-    ("M-S-<Print>", spawn "scrot -s -q 100 ~/Pictures/Screenshots/area-%Y-%m-%d-%H-%M-%S.png"),
-    -- Increment brightness by 10%
-    ("M-<Page_Up>", spawn "xbacklight -inc 10"),
-    -- Decrement brightness by 10%
-    ("M-<Page_Down>", spawn "xbacklight -dec 10"),
-    -- Switch to the next workspace
-    ("M-C-j", nextWS),
-    -- Switch to the previous workspace
-    ("M-C-k", prevWS),
-    -- Move the focused window to the next workspace
-    ("M-C-S-j", C.shiftToNext),
-    -- Move the focused window to the previous workspace
-    ("M-C-S-k", C.shiftToPrev),
-    -- View next screen
-    ("M-C-l", C.nextScreen),
-    -- View prev screen
-    ("M-C-h", C.prevScreen),
-    -- Move focused window to workspace on next screen
-    ("M-C-S-l", C.shiftNextScreen),
-    -- Move focused window to workspace on prev screen
-    ("M-C-S-h", C.shiftPrevScreen),
-    -- Toggle to the workspace displayed previously
-    ("M-C-<Tab>", toggleWS),
-    -- Add dynamic WS group
-    ("M-M1-n", addGroup),
-    -- Go to dynamic WS group
-    ("M-M1-g", goToGroup),
-    -- Forget dynamic WS group
-    ("M-M1-f", forgetGroup),
-    -- Lock the screen
-    ("M-S-l", spawn "slock"),
-    -- Close focused window
-    ("M-S-c", kill),
-    -- Rotate through the available layout algorithms
-    ("M-<Space>", sendMessage NextLayout),
-    -- Resize viewed windows to the correct size
-    ("M-n", refresh),
-    -- Move focus to the next window
-    ("M-<Tab>", windows W.focusDown),
-    -- Move focus to the next window
-    ("M-j", windows W.focusDown),
-    -- Move focus to the previous window
-    ("M-k", windows W.focusUp),
-    -- Move focus to the master window
-    ("M-m", windows W.focusMaster),
-    -- Swap the focused window and the master window
-    ("M-<Return>", windows W.swapMaster),
-    -- Swap the focused window with the next window
-    ("M-S-j", windows W.swapDown),
-    -- Swap the focused window with the previous window
-    ("M-S-k", windows W.swapUp),
-    -- Shrink the master area
-    ("M-h", sendMessage Shrink),
-    -- Expand the master area
-    ("M-l", sendMessage Expand),
-    -- Push window back into tiling
-    ("M-t", withFocused $ windows . W.sink),
-    -- Increment the number of windows in the master area
-    ("M-,", sendMessage (IncMasterN 1)),
-    -- Decrement the number of windows in the master area
-    ("M-.", sendMessage (IncMasterN (-1))),
-    -- Toggle the status bar gap
-    ("M-b", sendMessage ToggleStruts),
-    -- Restart xmonad
-    ("M-q", spawn "xmonad --recompile; xmonad --restart")
-  ]
+myKeys = coreKeys ++ scratchPadKeys ++ controlKeys ++ cycleWSKeys ++ dynamicWSGroupKeys
+  where
+    coreKeys =
+      [ ("M-C-<Return>", spawn myTransparentTerminal),
+        ("M-S-<Return>", spawn myTerminal),
+        ("M-S-f", spawn "firefox --private-window"),
+        ("M-S-g", spawn "brave --incognito"),
+        ("M-f", spawn "firefox"),
+        ("M-g", spawn "brave"),
+        ("M-p", shellPrompt myPromptConfig)
+      ]
+
+    scratchPadKeys =
+      [ ("M-M1-<Return>", openScratchPad "terminal"),
+        ("M-S-a", openScratchPad "telegram"),
+        ("M-S-s", openScratchPad "slack"),
+        ("M-S-t", openScratchPad "htop"),
+        ("M-d", openScratchPad "ranger"),
+        ("M-s", openScratchPad "mixer")
+      ]
+
+    controlKeys =
+      [ ("M-<Page_Down>", spawn "xbacklight -dec 10"),
+        ("M-<Page_Up>", spawn "xbacklight -inc 10"),
+        ("M-<Print>", spawn "scrot -q 100 ~/Pictures/Screenshots/screen-%Y-%m-%d-%H-%M-%S.png"),
+        ("M-C-<Print>", spawn "scrot -u -q 100 ~/Pictures/Screenshots/window-%Y-%m-%d-%H-%M-%S.png"),
+        ("M-S-<Print>", spawn "scrot -s -q 100 ~/Pictures/Screenshots/area-%Y-%m-%d-%H-%M-%S.png"),
+        ("M-S-l", spawn "slock"),
+        ("M-b", sendMessage ToggleStruts)
+      ]
+
+    cycleWSKeys =
+      [ ("M-C-<Tab>", toggleWS),
+        ("M-C-S-h", C.shiftPrevScreen),
+        ("M-C-S-j", C.shiftToNext),
+        ("M-C-S-k", C.shiftToPrev),
+        ("M-C-S-l", C.shiftNextScreen),
+        ("M-C-h", C.prevScreen),
+        ("M-C-j", nextWS),
+        ("M-C-k", prevWS),
+        ("M-C-l", C.nextScreen)
+      ]
+
+    dynamicWSGroupKeys =
+      [ ("M-M1-f", forgetGroup),
+        ("M-M1-g", goToGroup),
+        ("M-M1-n", addGroup)
+      ]
 
 myRemovedKeys :: [String]
 myRemovedKeys =
   [ "M-S-q"
   ]
+
+myKeysConfig :: XConfig a -> XConfig a
+myKeysConfig config = config `additionalKeysP` myKeys `removeKeysP` myRemovedKeys
 
 -- Layouts
 defaultTall = Tall 1 0.05 0.5
@@ -265,12 +221,20 @@ slackScratchPad = NS "slack" spawn find manage
     find = className =? "Slack"
     manage = nonFloating
 
+telegramScratchPad :: NamedScratchpad
+telegramScratchPad = NS "telegram" spawn find manage
+  where
+    spawn = "telegram-desktop"
+    find = className =? "TelegramDesktop"
+    manage = nonFloating
+
 myScratchPads :: [NamedScratchpad]
 myScratchPads =
   [ htopScratchPad,
     mixerScratchPad,
     rangerScratchPad,
     slackScratchPad,
+    telegramScratchPad,
     terminalScratchPad
   ]
 
@@ -323,7 +287,7 @@ toggleWS = C.toggleWS' ignoredWorkspaces
 main :: IO ()
 main = do
   xMobar <- spawnPipe "xmobar -x 0"
-  xmonad $ docks (defaultSettings xMobar `additionalKeysP` myKeys `removeKeysP` myRemovedKeys)
+  xmonad $ docks (defaultSettings xMobar & myKeysConfig)
 
 xmobarPrettyPrinting :: Handle -> X ()
 xmobarPrettyPrinting xMobar =
