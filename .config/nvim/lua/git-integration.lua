64084c108475085ef40aa288e6cc4380c8a6f55e
kolyamba2105 <haravetskyy@gmail.com>
2021-11-29 15:19:31 +0200

    Neovim - Setup git-signs plugin

--- .config/nvim/lua/git-integration.lua
+++ .config/nvim/lua/git-integration.lua
@@ -8,3 +8,13 @@ vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>Git<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>gf', '<cmd>diffget //2<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>gj', '<cmd>diffget //3<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>gx', '<cmd>Git blame<CR>', opts)
+
+require('gitsigns').setup {
+  signs = {
+    add =           { hl = 'GitSignsAdd',     text = '+', numhl = 'GitSignsAddNr',    linehl = 'GitSignsAddLn' },
+    change =        { hl = 'GitSignsChange',  text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete =        { hl = 'GitSignsDelete',  text = '-', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete =     { hl = 'GitSignsDelete',  text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete =  { hl = 'GitSignsChange',  text = '_', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+  },
+}
