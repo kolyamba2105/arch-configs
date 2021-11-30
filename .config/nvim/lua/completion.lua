1837f396c4819059b20d95552d3abb0e8a2b81f3
kolyamba2105 <haravetskyy@gmail.com>
2021-11-30 14:21:59 +0200

    Neovim - Setup snippets

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -1,4 +1,5 @@
 local cmp = require('cmp')
+local snip = require('luasnip')
 
 cmp.setup {
   mapping = {
@@ -7,16 +8,30 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.close(),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-y>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+    ['<Tab>'] = function (fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif snip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function (fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif snip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
   },
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'luasnip' },
     { name = 'path' },
     { name = 'buffer' },
   },
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+  snippet = { expand = function(args) snip.lsp_expand(args.body) end },
 }
-
