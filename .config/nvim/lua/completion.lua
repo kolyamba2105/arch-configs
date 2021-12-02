6f88e3fe5dd22464b06a944f527b8b319d5b277f
kolyamba2105 <haravetskyy@gmail.com>
2021-12-02 07:37:48 +0200

    Neovim - Update completion config

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -12,7 +12,7 @@ cmp.setup {
       if cmp.visible() then
         cmp.select_next_item()
       elseif snip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+        snip.expand_or_jump()
       else
         fallback()
       end
@@ -21,7 +21,7 @@ cmp.setup {
       if cmp.visible() then
         cmp.select_prev_item()
       elseif snip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+        snip.jump(-1)
       else
         fallback()
       end
