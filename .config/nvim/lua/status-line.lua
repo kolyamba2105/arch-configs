9b3c8e2d29636e358fd9231431928c36d2e3a634
kolyamba2105 <haravetskyy@gmail.com>
2021-12-11 00:55:41 +0200

    Neovim - Update LuaLine config

--- .config/nvim/lua/status-line.lua
+++ .config/nvim/lua/status-line.lua
@@ -15,7 +15,7 @@ local setup_config = function ()
   table.insert(config.sections.lualine_b, { 'branch' })
   table.insert(config.sections.lualine_b, { 'diff' })
   table.insert(config.sections.lualine_c, { 'filename' })
-  table.insert(config.sections.lualine_x, { 'diagnostics', sources = { 'nvim_lsp' } })
+  table.insert(config.sections.lualine_x, { 'diagnostics', sources = { 'nvim_diagnostic' } })
   table.insert(config.sections.lualine_x, { 'filetype' })
 
   return config
