23af7e5b94f6b26ac8bff069aeacc41fac7c9eb1
kolyamba2105 <haravetskyy@gmail.com>
2022-01-02 14:34:54 +0200

    Neovim - Prepend telescrope and lsp keymappings with <leader> (trial)

--- .config/nvim/lua/tele-scope.lua
+++ .config/nvim/lua/tele-scope.lua
@@ -1,13 +1,13 @@
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap('n', 'fc', '<cmd>Telescope command_history<CR>', opts)
-vim.api.nvim_set_keymap('n', 'ff', '<cmd>Telescope find_files<CR>', opts)
-vim.api.nvim_set_keymap('n', 'fg', '<cmd>Telescope live_grep<CR>', opts)
-vim.api.nvim_set_keymap('n', 'fh', '<cmd>Telescope help_tags<CR>', opts)
-vim.api.nvim_set_keymap('n', 'fj', '<cmd>Telescope jumplist<CR>', opts)
-vim.api.nvim_set_keymap('n', 'fm', '<cmd>Telescope marks<CR>', opts)
-vim.api.nvim_set_keymap('n', 'fs', '<cmd>Telescope search_history<CR>', opts)
-vim.api.nvim_set_keymap('n', 'ft', '<cmd>Telescope file_browser<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tc', '<cmd>Telescope command_history<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>Telescope find_files<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tg', '<cmd>Telescope live_grep<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>th', '<cmd>Telescope help_tags<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tj', '<cmd>Telescope jumplist<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tm', '<cmd>Telescope marks<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>ts', '<cmd>Telescope search_history<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>Telescope file_browser<CR>', opts)
 
 require('telescope').setup {
   pickers = {
